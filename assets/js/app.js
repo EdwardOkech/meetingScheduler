3789be18364b5670731224df3ba35d2f89e2f57a
EdwardOkech <okechjobs@gmail.com>
2016-02-19 16:40:27 +0300

    Added more days in the day slot

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,11 +29,51 @@ angular.module('scheduleApp', ['firebase'])
 					0110:{
 						time: '11:00am',
 						booked: false
-					}
+					},
+					
 				}
 			},
 			tuesday: {
                 name: 'Tuesday',
+                slots: {
+                    0900: {
+                        time: '9:00am',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11:00am',
+                        booked: false
+                    }
+                }
+            },
+            wednesday: {
+                name: 'Wednesday',
+                slots: {
+                    0900: {
+                        time: '9:00am',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11:00am',
+                        booked: false
+                    }
+                }
+            },
+            thursday: {
+                name: 'Thursday',
+                slots: {
+                    0900: {
+                        time: '9:00am',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11:00am',
+                        booked: false
+                    }
+                }
+            },
+            friday: {
+                name: 'Friday',
                 slots: {
                     0900: {
                         time: '9:00am',
